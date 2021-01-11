e37352aafb254e15994bba054d1c367d103729a4
quintala <67744861+quintala@users.noreply.github.com>
2021-01-11 08:52:33 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,2 @@
 install.packages("tidyverse") ## przetwarzanie
-install.packages("data.table") ## do ramek danych
 install.packages("readxl") ## wczytanie plikow excela
-install.packages("sf") ## wczytanie plikow z mapami
-
